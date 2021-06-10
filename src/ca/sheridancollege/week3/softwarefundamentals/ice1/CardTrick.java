f024fa308add02d4dd1f9fa932f16b47e2fc26dd
Vikrant110 <85509022+Vikrant110@users.noreply.github.com>
2021-06-10 07:40:54 +0530

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,3 +1,5 @@
+/*Name =Vikrant Singh Choudhary
+* student ID : 991635492
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -22,6 +24,9 @@ public class CardTrick {
         System.out.println("The cards in the magic hand are: ");
         for (int i=0; i<magicHand.length; i++)
         {
+            card LuckyCard = new Card();
+            LuckyCard.setValue(7);
+            
             Card c = new Card();
             c.setValue(1 + (int) Math.floor(Math.random()*13));
             c.setSuit(Card.SUITS[(int)Math.floor(Math.random()*4)]);
